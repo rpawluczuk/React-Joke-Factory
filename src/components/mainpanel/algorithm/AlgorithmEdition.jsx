df26af39131cc1f92f4a419faa68ef9ef17e5c56
Radosław Pawluczuk <rpawluczuk@efinity.com>
2023-04-25 18:28:19 +0200

    Diagram edition works

--- src/components/mainpanel/algorithm/AlgorithmEdition.jsx
+++ src/components/mainpanel/algorithm/AlgorithmEdition.jsx
@@ -1,8 +1,11 @@
-import React, {useEffect, useState}  from 'react';
+import React, {useEffect, useState} from 'react';
 import {Form, FormGroup} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
+import DiagramCreation from "components/mainpanel/algorithm/algorithmcreation/DiagramCreation";
+import AlgorithmForm from "components/mainpanel/algorithm/algorithmcreation/AlgorithmForm";
+import AlgorithmFormButtons from "components/mainpanel/algorithm/algorithmcreation/AlgorithmFormButtons";
 
 const AlgorithmEdition = () => {
 
@@ -10,6 +13,7 @@ const AlgorithmEdition = () => {
     const [algorithmDto, setAlgorithmDto] = useState({
         name: '',
         description: '',
+        diagramBlockList: [{title: "", description: "", position: 0}]
     })
     const params = useParams();
     const [isBtnDisabled, setIsBtnDisabled] = useState(true)
@@ -26,20 +30,21 @@ const AlgorithmEdition = () => {
             setAlgorithmDto({
                 id: params.id,
                 name: res.data.name,
-                description: res.data.description
+                description: res.data.description,
+                diagramBlockList: res.data.diagramBlockList
             })
         })
     }, [])
 
-    function handleNameChange (event) {
+    function handleNameChange(event) {
         setAlgorithmDto({...algorithmDto, name: event.target.value})
     }
 
-    function handleDescriptionChange (event) {
+    function handleDescriptionChange(event) {
         setAlgorithmDto({...algorithmDto, description: event.target.value})
     }
 
-    function handleFormSubmit (event) {
+    function handleFormSubmit(event) {
         event.preventDefault()
         axios.put(`http://localhost:8082/api/algorithms`, algorithmDto)
             .then(navigate(`/algorithm-list`))
@@ -48,37 +53,25 @@ const AlgorithmEdition = () => {
     return (
         <div className="container">
             <p className="Data-header">Edit a new algorithm</p>
-
-            <Form onSubmit={handleFormSubmit} className='mt-4'>
-                <Form.Group className="mb-3">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text"
-                                  placeholder="Title"
-                                  onChange={handleNameChange}
-                                  value={algorithmDto.name}/>
-                    {nameMessage && <div className='Validation-message'>{nameMessage}</div>}
-                </Form.Group>
-                <FormGroup className="mb-3">
-                    <Form.Label>Content</Form.Label>
-                    <Form.Control as="textarea"
-                                  rows={6}
-                                  onChange={handleDescriptionChange}
-                                  value={algorithmDto.description}
-                                  placeholder="content"/>
-                </FormGroup>
-                <div className="d-flex flex-row-reverse">
-                    <Button className="float-right mx-2"
-                            variant="success"
-                            type="submit"
-                            disabled={isBtnDisabled}>
-                        Update Algorithm
-                    </Button>
-                    <Button className="float-right mx-2"
-                            variant={"primary"}>
-                        Cancel
-                    </Button>
-                </div>
-            </Form>
+            <div className="Data-container">
+                <Form onSubmit={handleFormSubmit} className='mt-4'>
+                    <div className="d-flex flex-row">
+                        <div className="d-flex flex-column col-6 p-4">
+                            <AlgorithmForm
+                                algorithmDto={algorithmDto}
+                                handleNameChange={handleNameChange}
+                                handleDescriptionChange={handleDescriptionChange}
+                                nameMessage={nameMessage}
+                            />
+                            <AlgorithmFormButtons isBtnDisabled={isBtnDisabled}/>
+                        </div>
+                        <DiagramCreation
+                            diagramBlockList={algorithmDto.diagramBlockList}
+                            setDiagramBlockList={(newList) => setAlgorithmDto(prevState => ({...prevState, diagramBlockList: newList}))}
+                        />
+                    </div>
+                </Form>
+            </div>
         </div>
     );
 };
